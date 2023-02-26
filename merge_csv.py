a1f0638678e8817888e93d135e43964c1342f03f
Margaret Schaub <margaretcschaub@outlook.com>
2023-02-26 11:13:36 -0700

    create merge csv

---
+++ merge_csv.py
@@ -0,0 +1,27 @@
+import os
+
+import pandas as pd
+
+
+def merge_csv_files(file_path, output_file_location):
+    file_list = os.listdir(file_path)
+    absolute_path_list = []
+    for file in file_list:
+        absolute_path = f'{file_path}/{file}'
+        absolute_path_list.append(absolute_path)
+
+    df_append = pd.DataFrame(columns=['URL', 'Item Name', 'Ingredients'])
+
+    li = []
+    for file in absolute_path_list:
+        df_temp = pd.read_csv(file, encoding="utf-8", index_col=None, header=0)
+        li.append(df_temp)
+        df_append = pd.concat(li, axis=0, ignore_index=True)
+        df_append = df_append.drop(columns=['Unnamed: 0'])
+
+    df_append.to_csv(output_file_location)
+
+
+if __name__ == "__main__":
+    merge_csv_files(r'/Users/margaretschaub/Desktop/merge_files',
+                    r'/Users/margaretschaub/Desktop/sunscreen_merged.csv')
