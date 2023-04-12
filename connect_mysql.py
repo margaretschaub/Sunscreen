6e267985443ad3543b4e91fef5aa7c031d3c2df4
Margaret Schaub <margaretcschaub@outlook.com>
2023-04-11 20:48:17 -0600

    load products

--- connect_mysql.py
+++ connect_mysql.py
@@ -10,6 +10,10 @@ foodland_info = foodland_info.drop(columns=['Unnamed: 0'])
 
 df = empdata.astype(object).where(pd.notnull(empdata), None)
 
+products = pd.read_csv(r'/Users/margaretschaub/Desktop/reef_safe_determination.csv', delimiter=',')
+products = products.drop(columns=['Unnamed: 0'])
+products_df = products.astype(object).where(pd.notnull(products), None).tail(-1)
+
 # try:
 #     conn = msql.connect(host='localhost', user='root',
 #                         password='maceee23')  # give ur username, password
@@ -28,33 +32,49 @@ df = empdata.astype(object).where(pd.notnull(empdata), None)
 # except Error as e:
 #     print("Error while connecting to MySQL", e)
 
+# try:
+#     conn = msql.connect(host='localhost', user='root',
+#                         password='maceee23')  # give ur username, password
+#     if conn.is_connected():
+#         cursor = conn.cursor()
+#         cursor.execute("USE sunscreen;")
+#         for i, row in df.iterrows():
+#             # here %S means string values
+#             sql = '''INSERT INTO sunscreen.foodland_inventory (brand, sku,
+#                         available,
+#                         product_name,
+#                         price,
+#                         unit_of_size,
+#                         size,
+#                         price_per_size,
+#                         previous_price,
+#                         discounted,
+#                         markdown_price,
+#                         markdown_label,
+#                         effective_from,
+#                         effective_until,
+#                         whole_price,
+#                         retailer_store_id) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)'''
+#             cursor.execute(sql, tuple(row))
+#             print("Record inserted")
+#             # the connection is not auto committed by default, so we must commit to save our changes
+#             conn.commit()
+# except Error as e:
+#     print("Error while connecting to MySQL", e)
+                        
 try:
     conn = msql.connect(host='localhost', user='root',
                         password='maceee23')  # give ur username, password
     if conn.is_connected():
         cursor = conn.cursor()
         cursor.execute("USE sunscreen;")
-        for i, row in df.iterrows():
+        for i, row in products_df.iterrows():
             # here %S means string values
-            sql = '''INSERT INTO sunscreen.foodland_inventory (brand, sku,
-                        available,
-                        product_name,
-                        price,
-                        unit_of_size,
-                        size,
-                        price_per_size,
-                        previous_price,
-                        discounted,
-                        markdown_price,
-                        markdown_label,
-                        effective_from,
-                        effective_until,
-                        whole_price,
-                        retailer_store_id) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)'''
+            sql = '''INSERT INTO sunscreen.products (url, item_name,
+            ingredients, reef_safe_determination) VALUES (%s,%s,%s,%s)'''
             cursor.execute(sql, tuple(row))
             print("Record inserted")
             # the connection is not auto committed by default, so we must commit to save our changes
             conn.commit()
 except Error as e:
     print("Error while connecting to MySQL", e)
-                        
