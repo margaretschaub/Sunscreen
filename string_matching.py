977d77404ff4dd3ca48de97114ca4698a4aa7e3f
Margaret Schaub <margaretcschaub@outlook.com>
2023-05-10 12:13:05 -0600

    formatting

--- string_matching.py
+++ string_matching.py
@@ -1,28 +1,47 @@
 from fuzzywuzzy import fuzz, process
 import pandas as pd
 
-foodland = pd.read_csv(r'/Users/margaretschaub/Desktop/foodland_needs_match.csv')
-product = pd.read_csv(r'/Users/margaretschaub/Desktop/master_products.csv')
 
-foodland_name = foodland['foodland_name'].tolist()
-product_name = product['item_name'].tolist()
+def read_csv(file_name):
+    csv = pd.read_csv(file_name)
+    return csv
 
-mat1 = []
-mat2 = []
-p = []
-threshold = 100
 
-##process.extractOne returns only one output which contains the string with the highest matching score
-for i in foodland_name:
-    mat1.append(process.extractOne(
-        i, product_name, scorer=fuzz.token_set_ratio))
-
-foodland['matches'] = mat1
-
-for j in foodland['matches']:
-    if j[1] >= threshold:
-        p.append(j[0])
-    mat2.append(",".join(p))
+def match_names(file_1, file_2, table, output_file):
+    mat1 = []
+    mat2 = []
     p = []
-foodland['matches'] = mat2
-foodland.to_csv(r'/Users/margaretschaub/Desktop/foodlandsunscreen_matches.csv')
+    threshold = 100
+
+    # process.extractOne returns only one output which contains the string with the highest matching score
+    for i in file_1:
+        mat1.append(process.extractOne(
+            i, file_2, scorer=fuzz.token_set_ratio))
+
+    table['matches'] = mat1
+
+    for j in table['matches']:
+        if j[1] >= threshold:
+            p.append(j[0])
+        mat2.append(",".join(p))
+        p = []
+    table['matches'] = mat2
+    table.to_csv(output_file)
+
+
+def main():
+    grocery_file = r'/Users/margaretschaub/Desktop/foodland_needs_match.csv'
+    product_file = r'/Users/margaretschaub/Desktop/master_products.csv'
+
+    foodland_df = read_csv(grocery_file)
+    product_df = read_csv(product_file)
+
+    foodland_name_list = foodland_df['foodland_name'].tolist()  # file 1
+    product_name_list = product_df['item_name'].tolist()  # file 2
+
+    output_file = r'/Users/margaretschaub/Desktop/foodlandsunscreen_matches.csv'
+    match_names(foodland_name_list, product_name_list, foodland_df, output_file)
+
+
+if __name__ == "__main__":
+    main()
